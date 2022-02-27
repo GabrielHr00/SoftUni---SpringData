7f707d5eba89aaf07255e29c641ea866eab789f1
gabrielh00 <a11914741@univie.ac.at>
2022-02-27 12:31:03 +0100

    Hibernate Lab

--- 03. Introduction to Hibernate/hibernateFirstLab/src/main/java/Main.java
+++ 03. Introduction to Hibernate/hibernateFirstLab/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import enitites.Student;
+import enitites.Teacher;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +12,13 @@ public class Main {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
+        Teacher teacher = new Teacher("Petka", LocalDate.now());
+        em.persist(teacher);
         Student student = new Student("gosho");
         em.persist(student);
+        
+        Student first = em.find(Student.class, 2);
+        em.remove(first);
 
         em.getTransaction().commit();
         em.close();
