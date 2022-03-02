777cbffc70e10c8b827dcc0812a6ce77918e0a59
gabrielh00 <a11914741@univie.ac.at>
2022-03-02 13:18:26 +0100

    Code First Ex

---
+++ 04. Code First Approach/codeFirstApproach/src/main/java/_04/_04Main.java
@@ -0,0 +1,17 @@
+package _04;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class _04Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("CodeFirstEx");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
